583b22ea0b44a129b0f2d9db945ac0ca3f8a338d
qhdxjh <63944579+qhdxjh@users.noreply.github.com>
2021-09-20 23:08:20 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -15,8 +15,8 @@ function load2D_bg(){
         timeout = window.requestAnimationFrame(function() {
             var yValue = calcValue(y, window.innerHeight);
             var xValue = calcValue(x, window.innerWidth);
-            
-                    
+
+
             background.style.backgroundPositionX = xValue * 1 -200+ "px ";
             background.style.backgroundPositionY =  (-yValue * 0.75-50 ) + "px";
         })
@@ -41,7 +41,7 @@ function load2D_bg2(){
         timeout = window.requestAnimationFrame(function() {
             var yValue = calcValue(y, window.innerHeight);
             var xValue = calcValue(x, window.innerWidth);
-            
+
             background.style.backgroundPositionX = xValue *1 -200+ "px ";
             background.style.backgroundPositionY =  (-yValue * 0.75-50 ) + "px";
         })
@@ -62,30 +62,6 @@ function getMousePos(event) {
     var scrollY = document.documentElement.scrollTop || document.body.scrollTop;
     var y = e.pageY || e.clientY + scrollY;
     return y;
-}
-
-function load2D_lihui(){
-    var background = document.getElementById("lihui");
-    var range = 40;
-    var calcValue = function calcValue(a, b) {
-        return (a / b * range - range / 2).toFixed(1);
-    };
-    var timeout = void 0;
-    document.addEventListener('mousemove',
-    function(_ref) {
-        var x = _ref.x,
-        y = _ref.y;
-        if (timeout) {
-            window.cancelAnimationFrame(timeout);
-        }
-        timeout = window.requestAnimationFrame(function() {
-            var xValue = calcValue(x, window.innerWidth);
-
-            background.style.left = xValue * 0.7-100 + "px";
-            
-        })
-    },false);
-
 }
 
 function load2D_yinghua(){
@@ -114,16 +90,16 @@ function load2D_yinghua(){
 var i=0;
 
 function aboutMe(){
-    var slogan = document.getElementById("slogan");
-    var slogan2 = document.getElementById("slogan2");
+    var introduce = document.getElementById("introduce");
+    var introduce2 = document.getElementById("introduce2");
     if(i==0){
         i=1;
-        slogan.style.height="100px";
-        slogan2.innerHTML="这里是高坂滑稽果，一个果厨~ 这个废柴能熟练掌握面向百度编程、Windows和Linux的开关机、PS和PR的安装和卸载、Arduino和各种零件的购买和损坏，还喜欢打音游（（（。。。生命不息，折腾不止！目前坐等高考中...";
+        introduce.style.height="100px";
+        introduce2.innerHTML="这里是";
     } else {
         i=0;
-        slogan.style.height="50px";
-        slogan2.innerHTML="如果奇迹有颜色，那么一定是橙色！";
+        introduce.style.height="50px";
+        introduce2.innerHTML="Nogame Nolife";
     }
-    
+
 }
